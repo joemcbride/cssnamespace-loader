a82748d76c604aa538ef0ec2f3ee86176afd5048
Joe McBride <joe@uicraftsman.com>
2015-08-03 12:36:20 -0700

    First

---
+++ README.md
@@ -0,0 +1,1 @@
+# CSS Namespace Loader for Webpack
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function(source) {
+  var ns = (this.options && this.options.ns) || '.bs3';
+  var result = ns + ' {\n' + source + '\n}';
+};
+
