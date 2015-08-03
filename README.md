917799e0953cb33ac959f2ba245ec2374ebf3caf
Joe McBride <joe@uicraftsman.com>
2015-08-03 12:46:37 -0700

    Second.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CSS Namespace Loader for Webpack
+
+A webpack loader that allows you to wrap source in a css namespace.
--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 module.exports = function(source) {
   var ns = (this.options && this.options.ns) || '.bs3';
   var result = ns + ' {\n' + source + '\n}';
+  return result;
 };
 
