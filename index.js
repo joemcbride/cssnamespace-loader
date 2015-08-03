8c5bf96be070113f50deed2ad5fc3585db09e0ea
Joe McBride <joe@uicraftsman.com>
2015-08-03 13:33:33 -0700

    Use loader-utils for options.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 
+var loaderUtils = require('loader-utils');
+
 module.exports = function(source) {
-  var ns = (this.options && this.options.ns) || '.bs3';
+  var query = loaderUtils.parseQuery(this.query);
+  var ns = '.bs3';
+
+  if(query.ns) {
+    ns = query.ns;
+  }
+
   var result = ns + ' {\n' + source + '\n}';
   return result;
 };
